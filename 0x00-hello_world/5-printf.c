e65b73a20fb0e1eb11b9ddd0e91a8e47e92f7ae8
Adedimeji <ojerindeadedimeji@gmail.com>
2022-09-08 19:56:14 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - prints a statement
+ * 
+ * Description: prints smth
+ * Return: returns zero
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
