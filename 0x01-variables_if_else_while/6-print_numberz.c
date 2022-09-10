5397271011e2ccbb67284fe5c26cb37e932be5b7
Adedimeji <ojerindeadedimeji@gmail.com>
2022-09-09 20:09:09 -0700

    6-print_numberz.c

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,12 +1,18 @@
 #include <stdio.h>
 
+/**
+ * main - prints zero to nine
+ *
+ * Return: returns zero
+ */
+
 int main(void)
 {
 	int i;
 
 	for (i = 0; i < 10; i++)
 	{
-		putchar(i);
+		putchar((i % 10) + '0');
 	}
 	putchar('\n');
 	return (0);
