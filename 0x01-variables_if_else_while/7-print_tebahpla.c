91aeaf474a7de48ea7816cb6086ce3e4de051864
Adedimeji <ojerindeadedimeji@gmail.com>
2022-09-09 20:16:53 -0700

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - print alphabets reversed in lowercase
+ *
+ * Return: returns zero
+ */
+
+int main(void)
+{
+	char c;
+	for (c = 'z'; c >= 'a'; c--)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+}
