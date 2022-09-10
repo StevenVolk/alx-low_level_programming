e1ee7f4839f80ac6fec22ffa7625daf93bb5fd06
Adedimeji <ojerindeadedimeji@gmail.com>
2022-09-09 19:39:27 -0700

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - prints all alphabets in lowercase and then uppercase
+ *
+ * Return: returns zero
+ */
+
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		putchar(c);
+	}
+
+	for (c = 'A'; c <= 'Z'; c++)
+	{
+		putchar(c);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
