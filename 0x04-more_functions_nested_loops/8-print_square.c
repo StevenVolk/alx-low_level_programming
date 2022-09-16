4b1b0723f89392bec7955563a0c04e33eda5a733
Adedimeji <ojerindeadedimeji@gmail.com>
2022-09-15 21:16:33 -0700

    8-print_square.c

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -10,13 +10,13 @@ void print_square(int size)
 {
 	int i;
 	int j;
-	
+
 	if (size <= 0)
 		_putchar('\n');
 
-	for (i = 0; i < n; i++)
+	for (i = 0; i < size; i++)
 	{
-		for (j = 0; j < n; j++)
+		for (j = 0; j < size; j++)
 		{
 			_putchar('#');
 		}
