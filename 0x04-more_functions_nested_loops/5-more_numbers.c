74d2c085b74d638bf973a0651fd5f2d835e2e1f1
Adedimeji <ojerindeadedimeji@gmail.com>
2022-09-15 20:03:42 -0700

    5-more_numbers.c

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints 1 to 14 ten times
+ */
+
+void more_numbers(void)
+{
+	int i;
+	int j;
+
+	for (i = '0'; i <= '10'; i++)
+	{
+		for (j = '0'; j <= '14'; i++)
+		{
+			_putchar(j);
+		}
+		_putchar('\n');
+	}
+	_putchar('\n');
+}
