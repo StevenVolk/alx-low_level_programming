23d9e33764b009c7e539f9a989fc98ec3e83a483
Adedimeji <ojerindeadedimeji@gmail.com>
2022-09-15 20:12:17 -0700

    5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -8,14 +8,17 @@ void more_numbers(void)
 {
 	int i;
 	int j;
+	int k;
 
-	for (i = '0'; i <= '10'; i++)
+	for (i = 0; i <= 10; i++)
 	{
-		for (j = '0'; j <= '14'; i++)
+		k = 0;
+
+		for (j = 0; j <= 14; i++)
 		{
-			_putchar(j);
+			_putchar(k);
+			k++;
 		}
 		_putchar('\n');
 	}
-	_putchar('\n');
 }
