4faf34215b94ede63251d4e20cf4778f6604163f
Adedimeji <ojerindeadedimeji@gmail.com>
2022-09-21 11:59:14 -0700

    6-puts2.c

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+
+/**
+ * _puts2 - writes to standard output
+ * @str: given string
+ */
+
+void _puts2(char *str)
+{
+	while (*str != '\0')
+	{
+		_putchar(*str);
+		str += 2;
+	}
+	_putchar('\n');
+}
