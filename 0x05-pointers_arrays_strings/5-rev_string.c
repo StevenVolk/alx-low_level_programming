d9d553f485b7b68b3ba271d67fe0bc0f154275e7
Adedimeji <ojerindeadedimeji@gmail.com>
2022-09-21 11:10:57 -0700

    5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -9,6 +9,8 @@
 void rev_string(char *s)
 {
 	int i = 0;
+	int j = 0;
+
 	char *temp = s;
 
 	while (*temp != '\0')
@@ -18,17 +20,19 @@ void rev_string(char *s)
 	}
 
 	i--;
-/*
-	while (j <= z)
+
+	while (j <= i)
 	{
 		_putchar(s[j]);
+		_putchar(j + '0');
 		j++;
 	}
 	_putchar('\n');
-*/
+
 	while (i >= 0)
 	{
 		_putchar(s[i]);
+		_putchar(i + '0');
 		i--;
 	}
 	_putchar('\n');
