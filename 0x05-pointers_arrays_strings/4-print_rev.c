4240ab1bd5ec8e666f8b2b97a832d3224b380773
Adedimeji <ojerindeadedimeji@gmail.com>
2022-09-21 09:40:04 -0700

    ..

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_rev - prints the reverse of a string
+ * @s: given string
+ */
+
+void print_rev(char *s)
+{
+	len = _strlen(*s);
+
+	while (len > 0)
+	{
+		_putchar(s[len]);
+	}
+	_putchar('\n');
+}
