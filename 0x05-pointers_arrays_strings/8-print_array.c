6c5f549a814ea1e720e29fa7095b1601e85e8155
Adedimeji <ojerindeadedimeji@gmail.com>
2022-09-21 12:51:22 -0700

    8-print_array.c

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -9,11 +9,20 @@
 
 void print_array(int *a, int n)
 {
-	for (int i = 0; i < n; i++)
+	int i;
+
+	for (i = 0; i < n; i++)
 	{
-		_putchar(a[i] + '0');
-		_putchar(',');
-		_putchar(' ');
+		if (i < n - 1)
+		{
+			_putchar(a[i] + '0');
+			_putchar(',');
+			_putchar(' ');
+		}
+		else
+		{
+			_putchar(a[i] + '0');
+		}
 	}
 	_putchar('\n');
 }
