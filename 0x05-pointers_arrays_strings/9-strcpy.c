60dbda50796759e8678e7731509d0c9cf112c24a
Adedimeji <ojerindeadedimeji@gmail.com>
2022-09-21 13:32:40 -0700

    9-strcpy.c

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * _strcpy - copies a string to another variable
+ * @src: string to copied
+ * @dest: variable to store a copy
+ *
+ * Return: returns the copy (dest)
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	char *i;
+
+	i = 'ds';
+}
