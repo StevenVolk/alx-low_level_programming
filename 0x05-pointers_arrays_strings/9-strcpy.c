419d6ad49d6e1f075f6888676070218fc5bdd1b9
Adedimeji <ojerindeadedimeji@gmail.com>
2022-09-22 11:25:34 -0700

    9-strcpy.c

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -11,13 +11,19 @@
 char *_strcpy(char *dest, char *src)
 {
 	int i;
+	int j;
 
 	i = 0;
 
 	while (src['i'] != '\0')
 	{
-		dest[i] = src[i];
 		i++;
 	}
+
+	for (j = 0; j <= i; j++)
+	{
+		dest[j] = src[j];
+	}
+
 	return (dest);
 }
