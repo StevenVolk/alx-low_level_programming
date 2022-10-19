30489cce9e3a3e457fe258911a004d452b4cdcff
Adedimeji <ojerindeadedimeji@gmail.com>
2022-10-19 12:09:50 -0700

    4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,6 +1,7 @@
 #include "main.h"
 
-/** _isalpha - checks if alhpabet, return 1 if so and 0 otherwise
+/**
+ * _isalpha - checks if alhpabet, return 1 if so and 0 otherwise
  *
  * Return: 1 for alphabet and 0 for others
  */
