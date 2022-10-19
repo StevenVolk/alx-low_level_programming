c5a505189d0a1da9fcf3488feff7bec2ecaa52bf
Adedimeji <ojerindeadedimeji@gmail.com>
2022-10-19 08:45:57 -0700

    2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_alphabet - print all alphabets in lower case
+ *
+ * Return: returns zero
+ */
+
+void print_alphabet_x10(void)
+{
+	for (int i = 0; i <= 10; i++)
+	{
+		char c;
+
+		for (c = 'a'; c <= 'z'; c++)
+		{
+			_putchar(c);
+		}
+		_putchar('\n');
+	}
+}
