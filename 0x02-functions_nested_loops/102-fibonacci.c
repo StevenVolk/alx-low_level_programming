c8b2830ee683ec361df9b09eacb1ce6c630f0fab
Adedimeji <ojerindeadedimeji@gmail.com>
2022-10-21 06:41:22 -0700

    102-fibonacci.c

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,22 @@
+#include <stdio>
+
+/**
+ * main - prints the first 50 fibonnachi number, starting with 1 and 2
+ *
+ * Return: Always 1
+ */
+
+int main(void)
+{
+	int i, former, current;
+
+	former = 0;
+	current = 1;
+	for (i = 0; i < 50; i++)
+	{
+		current += former;
+		printf("%i,", current);
+		former = current;
+	}
+	printf("\n");
+}
