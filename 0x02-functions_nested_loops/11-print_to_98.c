630dc1b2526ace71821333fdfc06c6060e63e4a5
Adedimeji <ojerindeadedimeji@gmail.com>
2022-10-21 02:39:19 -0700

    11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_to_98 - prints from any number to 98
+ * print_to_98 - print from a number to 98
  * @n: number to print from
  *
  * Return: returns void
@@ -9,77 +9,13 @@
 
 void print_to_98(int n)
 {
-	int i, j, k, l, m;
-
-	if (n <= 98)
+	while (n != 98)
 	{
-		for (i = n; i <= 98; i++)
-		{
-			if (i < 0)
-			{
-				m = i * -1;
-				_putchar('-');
-			}
-			else
-				m = i;
-			if (m < 10)
-			{
-				_putchar('0' + m);
-			}
-			else if (m < 100)
-			{
-				j = m / 10;
-				k = m % 10;
-				_putchar('0' + j);
-				_putchar('0' + k);
-			}
-			else
-			{
-				j = i % 10;
-				k = i / 10;
-				l = k % 10;
-				m = k / 10;
-				_putchar('0' + m);
-				_putchar('0' + l);
-				_putchar('0' + j);
-			}
-			if (i != 98)
-			{
-				_putchar(',');
-				_putchar(' ');
-			}
-			else
-				_putchar('\n');
-		}
+		printf("%i, ", n);
+		if (n < 98)
+			n++;
+		else
+			n--;
 	}
-	else
-	{
-		for (i = n; i >= 98; i--)
-		{
-			if (i >= 100)
-			{
-				j = i % 10;
-				k = i / 10;
-				l = k % 10;
-				m = k / 10;
-				_putchar('0' + m);
-				_putchar('0' + l);
-				_putchar('0' + j);
-			}
-			else
-			{
-				j = i / 10;
-				k = i % 10;
-				_putchar('0' + j);
-				_putchar('0' + k); 
-			}
-			if (i != 98)
-			{
-				_putchar(',');
-				_putchar(' ');
-			}
-			else
-				_putchar('\n');
-		}
-	}
+	printf("98\n");
 }
