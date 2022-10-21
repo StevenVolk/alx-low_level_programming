cf3f5d2e4c72e2fe126a3912698b1a6ff1632da6
Adedimeji <ojerindeadedimeji@gmail.com>
2022-10-21 07:41:54 -0700

    104-fibonacci.c

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - prints first 98 fibonnaci numbers
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int i;
+	double previous, present, future;
+
+	previous = 0;
+	present = 1;
+	for (i = 0, i < 98, i++)
+	{
+		future = previous + present;
+		previous = present;
+		present = future;
+		printf("%.0f", present);
+		if (i != 97)
+			printf(", ");
+	}
+	printf("\n");
+	return (0);
+}
