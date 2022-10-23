3ba181cee67e2cf73741c38ee1d15bcf70c3db86
Adedimeji <ojerindeadedimeji@gmail.com>
2022-10-23 03:43:57 -0700

    10-print_triangle.c

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -9,9 +9,9 @@ void print_triangle(int size)
 {
 	int i, j, k;
 
-	for (i = 0; i < size; i++)
+	for (i = 1; i <= size; i++)
 	{
-		for (j = 0; j < size; j++)
+		for (j = 1; j <= size; j++)
 		{
 			k = size - i;
 			if (j < k)
