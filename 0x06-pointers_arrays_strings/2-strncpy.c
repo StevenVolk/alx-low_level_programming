e28e8da81213a0d10b58c8f9c9aa6aa055c61a78
Adedimeji <ojerindeadedimeji@gmail.com>
2022-10-26 11:20:29 -0700

    2-strncpy.c

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,26 @@
+/**
+ * strncpy - copy first n characters from src to first n characters of dest
+ * @dest: destination of first n characters
+ * @src: source of first n characters to be copied
+ * @n: number of characters to be copied
+ *
+ * Return: returns dest
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	i = 0;
+	while (dest[i] != '\0')
+	{
+		if (src[i] == '\0')
+		{
+			dest[i] = '\0';
+		}
+		else if (i < n)
+		{
+			dest[i] = src[i];
+		}
+	}
+}
