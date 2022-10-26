d7310e2a44c2827a7a146a7d756cf6abaedc7f70
Adedimeji <ojerindeadedimeji@gmail.com>
2022-10-26 12:50:46 -0700

    3-strcmp.c

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,12 @@
+/**
+ * _strcmp - compares string; similar to strcmp
+ * @s1: first string to be compared
+ * @s2: second string to be compared
+ *
+ * Return: ASCII difference between the first character of s1 and s2
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	return (s1[0] - s2[0]);
+}
