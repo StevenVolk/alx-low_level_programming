b19dffadfcd617d765b5e9da339a4329a7c3c3c8
Adedimeji <ojerindeadedimeji@gmail.com>
2022-10-26 13:41:00 -0700

    3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -12,9 +12,10 @@ int _strcmp(char *s1, char *s2)
 
 	i = 0;
 	diff = 0;
-	while ((s1[i] != '\0' || s2[i] != '\0') && diff == 0)
+	while ((s1 != '\0' || s2 != '\0') && diff == 0)
 	{
 		diff = s1[i] - s2[i];
+		i++;
 	}
 
 	return (diff);
