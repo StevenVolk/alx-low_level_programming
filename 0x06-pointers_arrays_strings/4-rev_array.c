1950ffadaf2c704b8f93942e6735953016dec6d8
Adedimeji <ojerindeadedimeji@gmail.com>
2022-10-26 13:50:00 -0700

    4-rev_array.c

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,20 @@
+/**
+ * reverse_array - reverses an array of interger
+ * @a: array of intergers
+ * @n: number of intergers to be swapped
+ *
+ * Return: void
+ */
+
+void reverse_array(int *a, int n)
+{
+	int i; k;
+
+	i = 0;
+	k = n / 2;
+	while (i != k)
+	{
+		a[i] = a[n - i - 1];
+		i++;
+	}
+}
