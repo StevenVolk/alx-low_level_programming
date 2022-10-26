42ec6b7a97fcda04b28e096d4a21560a36cebd84
Adedimeji <ojerindeadedimeji@gmail.com>
2022-10-26 13:35:41 -0700

    3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -8,5 +8,14 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	return (s1[0] - s2[0]);
+	int i, diff;
+
+	i = 0;
+	diff = 0;
+	while ((s1[i] != '\0' || s2[i] != '\0') && diff == 0)
+	{
+		diff = s1[i] - s2[i];
+	}
+
+	return (diff);
 }
