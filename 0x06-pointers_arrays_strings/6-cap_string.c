785260e66f8b72e8a63cced0e297941d1bf24d0f
Adedimeji <ojerindeadedimeji@gmail.com>
2022-10-27 17:05:27 -0700

    6-cap_string.c

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,29 @@
+/**
+ * cap_string - capitalize the first letter of every word
+ * @c: input string
+ *
+ * Return: returns the capitalized version of the input string
+ */
+
+char *cap_string(char *c)
+{
+	int i, j;
+	char s[] = {' ', '\t', '\n', ',', ';', '.', '!', '?', '"', '(', ')', '{', '}'};
+
+	i = 1;
+	j = 0;
+	while (c[i] != '\0')
+	{
+		while (s[j])
+		{
+			if (s[j] == c[i - 1]){
+				c[i] = c[i] - 32;
+				break;
+			}
+			j++;
+		}
+		i++;
+	}
+
+	return (c);
+}
