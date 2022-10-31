554edf9ba1b44bde5ed80325847f87e4d4a169b4
Adedimeji <ojerindeadedimeji@gmail.com>
2022-10-31 12:15:34 -0700

    2-strchr.c

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,30 @@
+/**
+ * _strchr - points to c in the s, or NULL if the character is not found
+ *
+ * @s: input string
+ * @c: input character
+ * Return: returns first occurence of character c to the end of string s
+ */
+
+char *_strchr(char *s, char c)
+{
+	int i, j;
+	char *d;
+
+	i = 0;
+	j = 0;
+	k = 0;
+	while (s[i])
+	{
+		if (s[i] == c || j == 1)
+		{
+			d[k] = s[i];
+			j = 1;
+			k++;
+		}
+		i++;
+	}
+	d[k] = '\n';
+
+	return (d);
+}
