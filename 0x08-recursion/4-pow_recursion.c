216e7938b994c6f312f53bcb12222156c01c24b4
Adedimeji <ojerindeadedimeji@gmail.com>
2022-11-03 12:40:39 -0700

    4-pow_recursion.c

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,5 +1,5 @@
 /**
- * _pow_recursion: a function that returns the value of x raised to the power of y
+ * _pow_recursion - returns the value of x raised to the power of y
  *
  * @x: base value
  * @y: power value
