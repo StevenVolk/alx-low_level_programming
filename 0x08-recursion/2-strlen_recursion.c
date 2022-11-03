0faca423194a73238a2ff9424b609953a73c8c46
Adedimeji <ojerindeadedimeji@gmail.com>
2022-11-03 12:14:56 -0700

    2-strlen_recursion.c

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -6,10 +6,10 @@
 
 int _strlen_recursion(char *s)
 {
-	int i = 0;
-
-	while (s[i])
-		i++;
-
-	return (i);
+	if (*s != '\0')
+	{
+		return (_strlen_recursion(s + 1) + 1);
+	}
+	else
+		return (0);
 }
