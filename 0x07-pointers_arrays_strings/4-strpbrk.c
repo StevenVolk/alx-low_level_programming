917473cc5bdb167bf8d3e047a2adde93becd7257
Adedimeji <ojerindeadedimeji@gmail.com>
2022-11-03 07:02:16 -0700

    4-strpbrk.c

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,29 @@
+/**
+ * _strpbrk - points to the first occurence of an acceptable bytes
+ *
+ * @s: string to be checked
+ * @accept: acceptable bytes
+ * 
+ * Return: the point of first occerence to end of s
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+	int i, j;
+
+	i = 0;
+	while (s[i])
+	{
+		j = 0;
+		while (accept[j])
+		{
+			if (s[i] == accept[j])
+			{
+				return (s);
+			}
+			j++;
+		}
+		i++;
+	}
+	return (NULL);
+}
