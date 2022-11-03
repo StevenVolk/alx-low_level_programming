16b5a3289aa402ab9b2680a9edf7d735df50045c
Adedimeji <ojerindeadedimeji@gmail.com>
2022-11-03 10:17:25 -0700

    main.h

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -6,5 +6,7 @@
 int _putchar(char c);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
+void print_chessboard(char (*a)[8]);
+void set_string(char **s, char *to);
 
 #endif /* MAIN_H */
