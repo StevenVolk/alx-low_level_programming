d17aec9a961ea8a2d6344c1ca1e0bc5cce0e3cca
Adedimeji <ojerindeadedimeji@gmail.com>
2022-11-03 08:50:42 -0700

    7-print_chessboard.c

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_chessboard - prints a chess board
+ *
+ * @a: number of rows
+ */
+
+void print_chessboard(char (*a)[8])
+{
+	int i, j;
+
+	for (i = 0; a[i]; i++)
+	{
+		for (j = 0; j < 8; j++)
+			_putchar(a[i]);
+		_putchar('\n');
+	}
+}
