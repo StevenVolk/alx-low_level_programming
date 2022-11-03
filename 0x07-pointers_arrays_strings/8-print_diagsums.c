51e4dc95cbec58a892fa561d38c179596b19ea28
Adedimeji <ojerindeadedimeji@gmail.com>
2022-11-03 09:57:38 -0700

    8-print_diagsums.c

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -21,7 +21,7 @@ void print_diagsums(int *a, int size)
 		i = i + size + 1;
 	}
 	printf("%d, ", sum1);
-	while (j < (size * size))
+	while (j < (size * size - 1))
 	{
 		sum2 += a[j];
 		j = j + size - 1;
