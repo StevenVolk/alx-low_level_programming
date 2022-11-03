70b400095c4471ef48b549704f1730179d6619c6
Adedimeji <ojerindeadedimeji@gmail.com>
2022-11-03 10:46:53 -0700

    main.h

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -4,9 +4,5 @@
 #include <stdio.h>
 
 int _putchar(char c);
-char *_memcpy(char *dest, char *src, unsigned int n);
-char *_strchr(char *s, char c);
-void print_chessboard(char (*a)[8]);
-void set_string(char **s, char *to);
 
 #endif /* MAIN_H */
