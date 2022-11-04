094da7cae8b2fe24a1d3b46630622439eacd99a5
Adedimeji <ojerindeadedimeji@gmail.com>
2022-11-04 08:59:56 -0700

    100-is_palindrome.c

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -21,7 +21,7 @@ int chk_length(char *s, int i)
  * @z: second index to compare with
  * Return: 1 if palindrome, otherwise 0
  */
-int chk_palindrome(char *s, int a, int b)
+int chk_palindrome(char *s, int a, int z)
 {
 	if (s[a] != s[z])
 		return (0);
