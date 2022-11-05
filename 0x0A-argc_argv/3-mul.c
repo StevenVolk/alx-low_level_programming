0dc948c650494de852bbbcb3bd471f368bd5f23c
Adedimeji <ojerindeadedimeji@gmail.com>
2022-11-05 00:27:39 -0700

    3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -11,7 +11,7 @@
 int main(int argc, char *argv[])
 {
 	if (argc != 2)
-		return ("%d\n", argv[1] * argv[2]);
+		return ("%d\n", (argv[1] * argv[2]));
 	else
 		return("%s\n", "Error");
 	return (1);
