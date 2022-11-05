1f0dfc330684ec5b7437c05d76644d7e4c6b8031
Adedimeji <ojerindeadedimeji@gmail.com>
2022-11-05 07:54:19 -0700

    0-create_array.c

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,20 @@
+/**
+ * create_array - creates array of int
+ * @size: size of array
+ * @c: character
+ * Return: NULL if size 0, otherwise pointer to array
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	int i;
+	char *arr[size];
+
+	if (size == 0)
+		return (NULL);
+
+	for (i = 0; i < size; i++)
+		arr[i] = c;
+
+	return (*arr);
+}
