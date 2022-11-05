081f3c3853b8ff6d940c77f92f0afe7ef076f803
Adedimeji <ojerindeadedimeji@gmail.com>
2022-11-05 15:17:22 -0700

    2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -17,11 +17,9 @@ char *str_concat(char *s1, char *s2)
 	i = 0;
 	j = 0;
 	k = 0;
-	if (s1 == NULL && s2 == NULL)
-		return (NULL);
-	else if (s1 == NULL)
+	if (s1 == NULL)
 		s1 = "";
-	else if (s2 == NULL)
+	if (s2 == NULL)
 		s2 = "";
 	while (s1[i])
 		i++;
