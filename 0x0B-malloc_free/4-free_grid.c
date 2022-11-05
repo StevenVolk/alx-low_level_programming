4ce85947c436bc923b51e255a19c104100fbaee9
Adedimeji <ojerindeadedimeji@gmail.com>
2022-11-05 16:41:38 -0700

    4-free_grid.c

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * free_grid - frees the grid
+ *
+ * @grid: array of 2-dimensional int
+ * @height: height
+ */
+
+void free_grid(int **grid, int height)
+{
+	int h;
+
+	for (h = 0; h < height; h++)
+		free(grid[h]);
+	free(grid);
+}
