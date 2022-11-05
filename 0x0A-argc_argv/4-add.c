af37b6549e0c7860108409105b41e3024f79d25b
Adedimeji <ojerindeadedimeji@gmail.com>
2022-11-05 01:51:33 -0700

    4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -14,11 +14,17 @@ int main(int argc, char *argv[])
 	int i, j;
 	int sum = 0;
 
+	if (argc < 2)
+	{
+		printf("0\n");
+		return (0);
+	}
+
 	for (i = 1; i < argc; i++)
 	{
 		for (j = 0; argv[i][j] != '\0'; j++)
 		{
-			if (argv[i][j] < 0 || argv[i][j] > 9)
+			if (argv[i][j] < '0' || argv[i][j] > '9')
 			{
 				printf("%s\n", "Error");
 				return (1);
@@ -27,6 +33,5 @@ int main(int argc, char *argv[])
 		sum += atoi(argv[i]);
 	}
 	printf("%d\n", sum);
-
 	return (0);
 }
