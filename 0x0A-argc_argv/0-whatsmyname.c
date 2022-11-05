83be4016a8489b852eabddfdcac6427e48e68b30
Adedimeji <ojerindeadedimeji@gmail.com>
2022-11-04 23:43:05 -0700

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * main - main function
+ *
+ * @argc: index for arguments
+ * @argv: array of arguements
+ * Return: Always 0
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s", argv[0]);
+	return (0);
+}
