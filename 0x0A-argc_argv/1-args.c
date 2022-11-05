2d5cdda6faedaf6be5d40b0e8af2ca32e6c8fcc9
Adedimeji <ojerindeadedimeji@gmail.com>
2022-11-04 23:59:51 -0700

    1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - main function
+ *
+ * @argc: index of arguements
+ * @argv: array of arguements
+ * Return: Always 0
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc);
+}
