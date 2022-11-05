351a865ad279940c0245203c8e6e23b92ccfc1ec
Adedimeji <ojerindeadedimeji@gmail.com>
2022-11-05 02:20:42 -0700

    100-change.c

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - main function
+ *
+ * @argc: number of arguements
+ * @argv: array of arguements
+ * Return: 1 if error, otherwise 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int j, count, amount;
+	int change[] = [25, 10, 5, 2, 1];
+	count = 0;
+
+	if (argc < 2)
+	{
+		printf("%s\n", "Error");
+		return (1);
+	}
+
+	amount = atoi(argv[1]);
+
+	if (amount < 0)
+	{
+		printf("%s\n", "Error");
+		return (1);
+	}
+	for (j = 0; j < 5; j++)
+	{
+		count += amount / change[j];
+		amount = amount % change[j];
+	}
+	return (0)
+}
