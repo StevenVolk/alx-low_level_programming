7530a64d314d20a1818261c4dd47a779dc0a6289
Adedimeji <ojerindeadedimeji@gmail.com>
2022-11-05 15:34:23 -0700

    3-alloc_grid.c

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,29 @@
+/**
+ * alloc_grid - creates a grid with intialized to 0
+ *
+ * @width: width of the grid
+ * @height: height of the grid
+ * Return: returns grid
+ */
+
+int **alloc_grid(int width, int height)
+{
+	int w, h;
+	int **grid;
+
+	if (width <= 0 || height <= 0)
+		return (NULL);
+
+	h = 0;
+	while (h < height)
+	{
+		w = 0;
+		while (w < width)
+		{
+			grid[h][w] = 0;
+			w++;
+		}
+		h++;
+	}
+	return (grid);
+}
