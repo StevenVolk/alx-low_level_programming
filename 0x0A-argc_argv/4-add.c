cbff0145b2d1ce1663ba560535b6c464d8a5cfcf
Adedimeji <ojerindeadedimeji@gmail.com>
2022-11-05 01:06:59 -0700

    4-add.c

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - main function
+ *
+ * @argc: number of arguements
+ * @argv: array of arguements
+ * Return: Always 1
+ */
+
+int main(int argc, char *argv)
+{
+	int i;
+	int sum = 0;
+
+	for (i = 1; i < argc; i++)
+	{
+		sum += atoi(argv[i]);
+	}
+
+	return (1);
+}
