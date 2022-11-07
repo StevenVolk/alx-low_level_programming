6c548a411c28e404c452744eb94958d6784a99e3
Adedimeji <ojerindeadedimeji@gmail.com>
2022-11-07 05:16:14 -0800

    100-realloc.c

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -19,14 +19,9 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		free(ptr);
 		return (NULL);
 	}
-	if (new_size > old_size)
+	if (new_size > old_size || ptr == NULL)
 	{
-		ptr += malloc((new_size - old_size) * sizeof(char));
-		return (ptr);
-	}
-	if (ptr == NULL)
-	{
-		ptr = malloc(new_size * sizeof(char));
+		ptr = malloc(new_size);
 		return (ptr);
 	}
 }
