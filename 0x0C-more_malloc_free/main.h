c9b6e03aa1466c0c58f18dddceb9d1e62197cd3f
Adedimeji <ojerindeadedimeji@gmail.com>
2022-11-07 08:09:12 -0800

    main.h

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -10,4 +10,4 @@ void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 
-eendif /* MAIN_H */
+#endif /* MAIN_H */
