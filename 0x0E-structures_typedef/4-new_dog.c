1051499f130fa7695f1a3b10b4445cf9227c4a57
Adedimeji <ojerindeadedimeji@gmail.com>
2022-11-07 17:21:45 -0800

    4-new_dog.c

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ * new_dog - store a copy of name and owner
+ * @name: name of new_dog
+ * @age: age of new_dog
+ * @owner: owner of new_dog
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	new_dog->name = name;
+	new_dog->age = age;
+	new_dog->owner = owner;
+	if (new_dog)
+		return (new_dog);
+	else
+		return (NULL);
+}
