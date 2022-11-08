8ce738e5769a6272fe6382d685ff776fabdd49fa
Adedimeji <ojerindeadedimeji@gmail.com>
2022-11-08 01:24:38 -0800

    5-free_dog.c

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -10,5 +10,14 @@
 void free_dog(dog_t *d)
 {
 	if (d)
+	{
+		d = malloc(d);
+		d->name = malloc(sizeof(d->name));
+		d->age = malloc(sizeof(d->age));
+		d->owner = malloc(sizeof(d->owner));
+		free(d->name);
+		free(d->age);
+		free(d->owner);
 		free(d);
+	}
 }
