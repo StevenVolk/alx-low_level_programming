4ebb890db03ede3effaf3638e1e76424d6be7500
Adedimeji <ojerindeadedimeji@gmail.com>
2022-11-07 17:35:56 -0800

    5-free_dog.c

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ * free_dog - frees dogs
+ * @d: a structure of dog
+ */
+
+void free_dog(dog_t *d)
+{
+	if (d)
+	{
+		free(d->name);
+		free(d->age);
+		free(d->owner);
+		free(d);
+	}
+}
