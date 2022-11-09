06ea2dbce56a2a1a80e1e8cedd60c2b267f897b4
Adedimeji <ojerindeadedimeji@gmail.com>
2022-11-09 09:50:06 -0800

    1-array_iterator.c

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -4,6 +4,7 @@
 /**
  * array_iterator - executes a function given as a parameter on each elemen
  *
+ * @array: an array
  * @size: size of the array
  * @action: a pointer to the function you need to use
  */
