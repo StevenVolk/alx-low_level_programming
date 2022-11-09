c5f61c02495c7fcf1e350dc46c2046729874c616
Adedimeji <ojerindeadedimeji@gmail.com>
2022-11-09 09:09:13 -0800

    0-print_name.c

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+/**
+ * print_name - prints a name
+ *
+ * @name: name to be printed
+ * @f: function to print the name
+ * Return: nothing
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (name == NULL || f == NULL)
+		return;
+	f(name);
+}
