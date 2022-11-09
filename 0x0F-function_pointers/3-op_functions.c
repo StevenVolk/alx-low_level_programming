8cb82e1a75da16bc007a6b3ed9968a27e4294e7e
Adedimeji <ojerindeadedimeji@gmail.com>
2022-11-09 10:55:49 -0800

    3

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include "calc.h"
+
+/**
+ * op_add - sum of two interger
+ * @a: first integer
+ * @b: second integer
+ * Return: sum of two integer
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
