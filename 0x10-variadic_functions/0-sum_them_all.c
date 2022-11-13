bb6f4cdd7705d57c808bddc514ca7abfac80f311
Adedimeji <ojerindeadedimeji@gmail.com>
2022-11-13 00:12:13 -0800

    0-sum_them_all.c

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdarg.h>
+
+/**
+ * sum_them_all - a function to sum all elements
+ * @n: integers
+ * Return: sum of all integers
+ */
+
+int sum_them_all(const unsigned int n, ...)
+{
+	va_list ap;
+	unsigned int i, sum = 0;
+
+	if (n == 0)
+		return (0);
+
+	va_start(ap, n);
+
+	for (i = 0; i < n, i++)
+		sum += va_arg(ap, int);
+
+	va_end(ap);
+	return (sum);
+}
