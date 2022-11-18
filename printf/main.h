841bbacd86801c390d2b6248517f9f014cd6edd7
Adedimeji <ojerindeadedimeji@gmail.com>
2022-11-17 21:39:13 -0800

    main.h

--- printf/main.h
+++ printf/main.h
@@ -3,6 +3,18 @@
 
 #include <stdio.h>
 
+/**
+ * struct op - Struct op
+ * @op: The operator
+ * @f: The function associated
+ */
+
+typedef struct op
+{
+	char *op;
+	void (*f)(va_list item);
+} op_t;
+
 int _putchar(char c);
 int _printf(const char *format, ...);
 
