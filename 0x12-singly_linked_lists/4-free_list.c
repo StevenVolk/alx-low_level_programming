27674c1bcc78b7d57a7fccd5575cdee74daa91f0
Adedimeji <ojerindeadedimeji@gmail.com>
2022-11-18 23:06:16 -0800

    4-free_list.c

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * free_list - frees a list_t list
+ * @head: head of singly linked list
+ */
+void free_list(list_t *head)
+{
+	list_t *node;
+
+	if (head == NULL)
+		return;
+	while (head)
+	{
+		node = head->next
+		free(head->str);
+		free(head);
+		head = node;
+	}
+}
