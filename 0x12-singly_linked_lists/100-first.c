f609a96c581f06d4505f1b7688a176bb1eb7e546
Adedimeji <ojerindeadedimeji@gmail.com>
2022-11-18 23:21:45 -0800

    100-first.c

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-void __attribute__((constructor))
+void __attribute__((constructor)) pre()
 {
 	printf("You're beat! and yet, you must allow,\n");
 	printf("I bore my house upon my back!\n");
