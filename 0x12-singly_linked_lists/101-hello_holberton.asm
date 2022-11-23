5f337bf884cb9d9c8c36200af0ebb1db52ec589b
Adedimeji <ojerindeadedimeji@gmail.com>
2022-11-22 16:17:44 -0800

    101-hello_holberton.asm

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,16 +1,16 @@
-section .text
+	section .text
 	default	rel
 	extern	printf
 	global	main
 main:
-	push rbp
-	mov rdi, fmt
-	mov rsi, message
-	mov rax, 0
-	call printf
-	pop rbp
-	mov rax, 0
+	push	rbp
+	mov	rdi, fmt
+	mov	rsi, message
+	mov	rax, 0
+	call	printf
+	pop	rbp
+	mov	rax, 0
 	ret
-session .data
-	message: db "Hello, Holberton", 10, 0
-	fmt: db "%s", 10, 0
+	session .data
+message: db "Hello, Holberton", 10, 0
+fmt: db "%s", 10, 0
