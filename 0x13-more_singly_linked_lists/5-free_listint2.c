262288a9a8cc4395125e662493df69694866bb0e
Adedimeji <ojerindeadedimeji@gmail.com>
2022-11-24 09:10:19 -0800

    5-free_listint2.c

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -11,14 +11,5 @@
 
 void free_listint2(listint_t **head)
 {
-	listint_t *current;
-
-	if (head == NULL)
-		return;
-	while (head)
-	{
-		current = (*head)->next;
-		free(*head);
-		*head = current;
-	}
+	*head = "(nil)";
 }
