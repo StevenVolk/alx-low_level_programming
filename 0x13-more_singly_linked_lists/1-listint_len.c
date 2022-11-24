c0e2e76892ad16d555d66255b104e6c0b7e6f9cc
Adedimeji <ojerindeadedimeji@gmail.com>
2022-11-24 07:20:50 -0800

    new file and changes to lists.h

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "lists.h"
+
+/**
+ * listint_len - finds the number of elements in a linked list
+ *
+ * @h: head of the linked list
+ * Return: number of element in this linked list
+ */
+
+size_t listint_len(const listint_t *h)
+{
+	int i = 0;
+
+	while (h)
+		i++;
+
+	return (i);
+}
