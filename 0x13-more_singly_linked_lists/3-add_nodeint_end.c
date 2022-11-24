c3f55acf344f79582e85f3d11d12424df650ffa0
Adedimeji <ojerindeadedimeji@gmail.com>
2022-11-24 08:50:25 -0800

    3

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -22,6 +22,13 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 		return (NULL);
 	new_node->n = n;
 	new_node->next = NULL;
+	if (new_node == NULL)
+		return (NULL);
+	if (*head == NULL)
+	{
+		*head = new_node;
+		return (*head);
+	}
 	while (head_cp->next)
 		head_cp = head_cp->next;
 	head_cp->next = new_node;
