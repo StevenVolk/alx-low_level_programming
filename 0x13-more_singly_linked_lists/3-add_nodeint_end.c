f7c8c87d82aaf9698eb7bc9c021d3ae4d68e193c
Adedimeji <ojerindeadedimeji@gmail.com>
2022-11-24 08:42:56 -0800

    3-add_nodeint_end.c

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -16,7 +16,7 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 	listint_t *new_node;
 	listint_t *head_cp;
 
-	heap_cp = *head;
+	head_cp = *head;
 	while (head_cp->next)
 		head_cp = head_cp->next;
 	new_node = malloc(sizeof(listint_t));
