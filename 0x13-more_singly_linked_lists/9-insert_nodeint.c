cbf95f58c086a1fedf836fd3c8e351ad49935e9d
Adedimeji <ojerindeadedimeji@gmail.com>
2022-11-25 08:08:06 -0800

    9-insert_nodeint.c

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -27,7 +27,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		*head = new_node;
 		return (new_node);
 	}
-	if (idx = 0)
+	if (idx == 0)
 	{
 		new_node->n = n;
 		new_node->next = node;
