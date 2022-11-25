5ef35ec0f1d612a4a3649f923015fae9e7d7331f
Adedimeji <ojerindeadedimeji@gmail.com>
2022-11-24 21:16:23 -0800

    4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -7,17 +7,45 @@
  * @name: name of new_dog
  * @age: age of new_dog
  * @owner: owner of new_dog
+ * Return: the new dog_t
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *new;
+	char *cp_name, *cp_owner;
+	int s;
+	int i = 0, j = 0;
+
+	while (name[i])
+		i++;
+
+	while (owner[j])
+		j++;
+
+	cp_name = malloc(i + 1);
+	if (cp_name == NULL)
+		return (NULL);
+	for (s = 0; name[s]; s++)
+	{
+		cp_name[s] = name[s];
+	}
+	cp_name = '\0';
+
+	cp_owner = malloc(j + 1);
+	if (cp_owner == NULL)
+		return (NULL);
+	for (s = 0; owner[s]; s++)
+	{
+		cp_owner[s] = owner[s];
+	}
+	cp_owner = '\0';
 
 	new = malloc(sizeof(dog_t));
 	if (new == NULL)
 		return (NULL);
-	new->name = name;
+	new->name = cp_name;
 	new->age = age;
-	new->owner = owner;
+	new->owner = cp_owner;
 	if (new)
 		return (new);
 	else
