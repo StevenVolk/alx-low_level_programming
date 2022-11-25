0ffe99f59926128eebd7d9d33f43369fbe73492c
Adedimeji <ojerindeadedimeji@gmail.com>
2022-11-24 20:47:25 -0800

    4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -9,10 +9,12 @@
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	&new_dog->name = name;
-	&new_dog->age = age;
-	&new_dog->owner = owner;
-	if (&new_dog)
+	struct dog new_dog;
+
+	new_dog->name = name;
+	new_dog->age = age;
+	new_dog->owner = owner;
+	if (new_dog)
 		return (&new_dog);
 	else
 		return (NULL);
