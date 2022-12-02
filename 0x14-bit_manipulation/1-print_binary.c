2f7c7c248965139d33dd3d5d6bdbcacc543de6ea
Adedimeji <ojerindeadedimeji@gmail.com>
2022-12-02 09:20:03 -0800

    changes

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -10,7 +10,7 @@ void print_binary(unsigned long int n)
 {
 	if (n == 0)
 	{
-		_putchar("0");
+		_putchar('0');
 		return;
 	}
 	else
@@ -29,7 +29,7 @@ void print_bin(unsigned long int n)
 		return;
 	print_bin(n >> 1);
 	if ((n & 1) == 1)
-		_putchar("1");
+		_putchar('1');
 	else
-		_putchar("0");
+		_putchar('0');
 }
