0596a95a666a501cf330cd7a882ad29a9939eea8
Adedimeji <ojerindeadedimeji@gmail.com>
2022-12-02 09:13:30 -0800

    1-print_binary.c

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+/**
+ * print_binary - prints the binary representation of a number
+ * @n: number in decimal
+ * Return: nothing
+ */
+
+void print_binary(unsigned long int n)
+{
+	if (n == 0)
+	{
+		_putchar("0");
+		return;
+	}
+	else
+		print_bin(n);
+}
+
+/**
+ * print_bin - prints binary representation
+ * @n: number in decimal
+ * Return: nothing
+ */
+
+void print_bin(unsigned long int n)
+{
+	if (n == 0)
+		return;
+	print_bin(n >> 1);
+	if ((n & 1) == 1)
+		_putchar("1");
+	else
+		_putchar("0");
+}
