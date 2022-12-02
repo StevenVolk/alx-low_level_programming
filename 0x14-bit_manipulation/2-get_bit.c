b4bc91c3ce046039ed87e0482b34f5bab4fa26fa
Adedimeji <ojerindeadedimeji@gmail.com>
2022-12-02 10:21:22 -0800

    2-get_bit.c

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -11,6 +11,8 @@ int get_bit(unsigned long int n, unsigned int index)
 {
 	unsigned int i;
 
+	if (index > (sizeof(8) * 8))
+		return (-1);
 	i = (n >> index) & 1;
 	if ((i == 1) || (i == 0))
 		return (i);
