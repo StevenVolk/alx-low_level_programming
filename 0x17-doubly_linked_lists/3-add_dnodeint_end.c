786652a9e3560a2239a8aa0b1d8c1db8738d82a7
Adedimeji <ojerindeadedimeji@gmail.com>
2022-12-16 08:35:29 -0800

    3-add_dnodeint_end.c

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -7,6 +7,7 @@
  * add_dnodeint_end - adds a new node at the end of a dlistint_t list
  * @head: head of list
  * @n: value of new node
+ * Return: head of the dlistint_t list
  */
 
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
