46672d62cd8684cd99849e1ee8ea8335b0dcf726
Adedimeji <ojerindeadedimeji@gmail.com>
2022-12-17 05:39:01 -0800

    8-delete_dnodeint.c

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -23,6 +23,8 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	if (index == 0)
 	{
 		current->next->prev = NULL;
+		*head = current->next;
+		free(current);
 		return (1);
 	}
 
