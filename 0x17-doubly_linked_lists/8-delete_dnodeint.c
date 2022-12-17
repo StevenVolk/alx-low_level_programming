153032a1f394f92d3bc3c5693e8b280c08475a6f
Adedimeji <ojerindeadedimeji@gmail.com>
2022-12-17 05:36:19 -0800

    8-delete_dnodeint.c

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -15,7 +15,7 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	dlistint_t *current = NULL;
 	unsigned int i = 0;
 
-	if (head == NULL || (*head) == NULL)
+	if (head == NULL || (*head) == NULL || index < 0)
 		return (-1);
 
 	current = *head;
