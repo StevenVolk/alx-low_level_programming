206300577680d77501122d4edceb912c4688db5c
Adedimeji <ojerindeadedimeji@gmail.com>
2022-12-24 18:12:52 -0800

    7-insert_dnodeint.c

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -43,11 +43,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	if (idx == 0)
 		return (add_dnodeint(h, n));
 	else if (length == idx)
-	{
-		if (n == 150)
-			printf("error");
 		return (add_dnodeint_end(h, n));
-	}
 	current_node = *h;
 	while (current_node != NULL)
 	{
