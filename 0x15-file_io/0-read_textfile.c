21367a7cc1286bcc99f47b7d2dc6dbee97f26bc6
Adedimeji Ojerinde <ojerindeadedimeji@gmail.com>
2023-05-02 18:08:42 -0700

    changes

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * reads a text file and prints it to the POSIX standard output.
+ * read_textfile - reads a text file and prints it to the POSIX standard output
  * @filename: name of text file
  * @letters: number of letters it should read and print
  * Return: 0 if filename can't be opened or read, or number of letters printed
@@ -17,7 +17,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		free(characters);
 		return (0);
 	}
-	
+
 	if (!filename)
 		return (0);
 
@@ -30,5 +30,5 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	write(STDOUT_FILENO, characters, read_out);
 	free(characters);
 	close(fd);
-	return(read_out);
+	return (read_out);
 }
