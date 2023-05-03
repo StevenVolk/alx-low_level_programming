63aa5fdb854fea62292d8477c82d388c4771ed83
Adedimeji Ojerinde <ojerindeadedimeji@gmail.com>
2023-05-03 12:35:27 -0700

    changes

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -9,13 +9,18 @@
 
 int create_file(const char *filename, char *text_content)
 {
-	int fd, len = 0;
+	int fd, fdw, len = 0;
 
 	if (filename == NULL)
 		return (-1);
 
 	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
 
+	if (text_content == NULL)
+	{
+		close(fd);
+		return (1);
+	}
 	if (fd == -1)
 		return (-1);
 
@@ -23,7 +28,9 @@ int create_file(const char *filename, char *text_content)
 		len++;
 
 	if (text_content)
-		write(fd, text_content, len);
+		fdw = write(fd, text_content, len);
+	if (fdw == -1)
+		return (-1);
 	close(fd);
 	return (1);
 }
--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -15,6 +15,11 @@ int append_text_to_file(const char *filename, char *text_content)
 		return (-1);
 
 	fd = open(filename, O_WRONLY | O_APPEND);
+	if (text_content == NULL)
+        {
+                close(fd);
+                return (1);
+        }
 	if (fd == -1)
 		return (-1);
 
