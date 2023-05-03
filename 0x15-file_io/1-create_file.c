e81bd15aaca717a0d2ccb98c25e6b6833b71104b
Adedimeji Ojerinde <ojerindeadedimeji@gmail.com>
2023-05-02 19:32:41 -0700

    changes to file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -14,7 +14,7 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
 
 	if (fd == -1)
 		return (-1);
@@ -22,8 +22,7 @@ int create_file(const char *filename, char *text_content)
 	while (text_content[len])
 		len++;
 
-	if (text_content)
-		write(fd, text_content, len);
+	write(fd, text_content, len);
 	close(fd);
 	return (1);
 }
