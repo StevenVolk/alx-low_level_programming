bceb83328ce30515faccee1b9b22671bcd9bb778
Adedimeji Ojerinde <ojerindeadedimeji@gmail.com>
2023-05-24 09:18:03 -0700

    correction

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -11,13 +11,13 @@ def island_perimeter(grid):
 
     for i in range(0, rows-1):
         for j in range(0, columns-1):
-            if grid[i, j] == 1:
-                if i == 0 or grid[i-1, j] == 0:
+            if grid[i][j] == 1:
+                if i == 0 or grid[i-1][j] == 0:
                     perimeter += 1
-                if i == rows-1 or grid[i+1, j] == 0:
+                if i == rows-1 or grid[i+1][j] == 0:
                     perimeter += 1
-                if j == 0 or grid[i, j-1] == 0:
+                if j == 0 or grid[i][j-1] == 0:
                     perimeter += 1
-                if j == columns-1 or grid[i, j+1] == 0:
+                if j == columns-1 or grid[i][j+1] == 0:
                     perimeter += 1
     return perimeter
