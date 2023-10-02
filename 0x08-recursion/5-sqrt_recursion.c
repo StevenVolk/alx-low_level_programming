f11c88fdc0cf7341ce77488536f707dc9791ccc2
Adedimeji Ojerinde <36421837+StevenVolk@users.noreply.github.com>
2023-10-02 19:19:46 -0400

    New file

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,34 @@
+/**
+ * chk_sqrt - checks for natural square root of a number n
+ *
+ * @n: number
+ * @i: number to check if a perfect root of n
+ * Return: the natural square root of a number
+ */
+
+int chk_sqrt(int n, int i)
+{
+	if (i * i < n)
+		return (chk_sqrt(n, (i + 1)));
+	else if (i * i == n)
+		return (i);
+	else
+		return (-1);
+}
+
+/**
+ * _sqrt_recursion - finds natural square root of a number
+ *
+ * @n: number
+ * Return: natural square root of a number
+ */
+
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+		return (-1);
+	else if (n == 0 || n == 1)
+		return (n);
+	else
+		return (chk_sqrt(n, 1));
+}
---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,32 @@
+/**
+ * chk_prime - checks if a number is prime
+ *
+ * @n: number to be checked
+ * @i: divisor
+ * Return: 1 if a prime number, otherwise 0
+ */
+
+int chk_prime(int n, int i)
+{
+	if ((i <= (n / 2)) && ((n % i) != 0))
+		return (chk_prime(n, i + 1));
+	else if (i > (n / 2))
+		return (1);
+	else
+		return (0);
+}
+
+/**
+ * is_prime_number - finds if a number is prime
+ *
+ * @n: number to be checked
+ * Return: 1 if a prime number, otherwise 0
+ */
+
+int is_prime_number(int n)
+{
+	if (n <= 1)
+		return (0);
+	else
+		return (chk_prime(n, 2));
+}
