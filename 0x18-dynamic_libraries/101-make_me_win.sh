0a050c569ffb0d8742e16c89b65591ea8b289046
Adedimeji Ojerinde <ojerindeadedimeji@gmail.com>
2023-12-11 06:15:16 -0800

    giga new 2

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P https://github.com/StevenVolk/alx-low_level_programming/raw/master/0x18-dynamic_libraries/solve.so
+wget -P /tmp https://github.com/StevenVolk/alx-low_level_programming/raw/master/0x18-dynamic_libraries/solve.so
 export LD_PRELOAD=/tmp/libwinner.so
